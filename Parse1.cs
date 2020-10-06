7c9503331f15d7ba2c88ae60abd95deaba52d5fe
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-10-06 14:08:22 +0530

    Add files via upload

---
+++ Parse1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace parse
+{
+    class Parse
+    {
+        static void Main(string[] args)
+        {
+          //The program matches and parses a number
+
+            string input = "c# 100 Trainig";
+            Match match = Regex.Match(input, @"\d+");
+            if (match.Success)
+            {
+                int.TryParse(match.Value, out int number);
+                // Show that we have the numbers.
+                Console.WriteLine("NUMBERS: {0}, {1}", number, number + 1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
