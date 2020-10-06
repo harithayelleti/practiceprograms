78a56babd0ce14959acbd1d82c50d663c6f9f868
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-10-06 13:12:58 +0530

    Add files via upload

---
+++ csharpregexnext1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace csharpregex3
+{
+    class csharpregexnext
+    {
+        static void Main(string[] args)
+        {
+            /*this is the example for regex by using nextmatch for metacharcaters*/
+
+            string value = "10 and 20 and 30 and 40 crazy 50 beautiful 60 programming 70 is fun 80";
+
+            // Step 1: get first match.
+            Match match = Regex.Match(value, @"\d+");
+            if (match.Success)
+            {
+                Console.WriteLine("Pattern Matched and the digits are: " + match.Value);
+            }
+
+            // Step 2: get second match.
+            match = match.NextMatch();
+            if (match.Success)
+            {
+                Console.WriteLine("The Second Executed and the digits are:" + match.Value);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
