d331d22daeb4f746b0118f9b5c7f087156c07ff8
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-10-06 13:27:12 +0530

    Add files via upload

---
+++ regexreplace1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace csharpregexreplace
+{
+    class regexreplace
+    {
+        static void Main(string[] args)
+        {
+            // Replace 2 or more digit pattern with a string.
+            Regex regex = new Regex(@"\D+");
+            string result = regex.Replace("cat and Dog", "789");
+            Console.WriteLine("The replacement of cat and dog is is {0}", result);
+            Console.ReadKey();
+        
+    }
+    }
+}
