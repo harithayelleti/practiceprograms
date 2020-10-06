d12f89985f9ea462c48ec6dec272cf155cb1a14c
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-10-06 13:21:14 +0530

    Add files via upload

---
+++ regexreplace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace csharpregexreplace
+{
+    class regexreplace
+    {
+        static void Main(string[] args)
+        {
+            // Replace 2 or more digit pattern with a string.
+            Regex regex = new Regex(@"\d+");
+            string result = regex.Replace("cat 123 456", "bird");
+            Console.WriteLine("RESULT: {0}", result);
+            Console.ReadKey();
+        
+    }
+    }
+}
