9f09e2e8460d3fb5489b4ce96c986949aa255cd0
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-10-06 13:04:03 +0530

    Add files via upload

---
+++ csharpregex5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace csharpregex3
+{
+    class csharpregex3
+    {
+        static void Main(string[] args)
+        {
+            /*this is the example for regex by using ismatch with start and ennd for metacharcaters*/
+            string test = "xxyy";
+
+            // Match the start of a string.
+            string text = "hari";
+            //Match the start of the string
+            if (Regex.IsMatch(text, ("ha|ri")))
+            {
+                Console.WriteLine("Pattern match,where string is starting with" + " "+text);
+            }
+
+            // Match the end of a string.
+            /*if (Regex.IsMatch(test, "yy$"))
+            {
+                Console.WriteLine("END MATCHES");
+            }*/
+            Console.ReadKey();
+        }
+    }
+}
