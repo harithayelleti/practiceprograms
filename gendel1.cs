c795b51f0d4566ba9ecb0fa8210223ddfe07a9b2
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-10-07 15:44:13 +0530

    Add files via upload

---
+++ gendel1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genericdel
+{
+    public delegate T multiply<T>(T m1, T m2); // generic delegate
+
+    class gendel
+    {
+        public static int Multiply(int pro1, int pro2)
+        {
+            return pro1 + pro2;
+        }
+
+        public static string Concat(string name1, string name2)
+        {
+            return name1 + name2;
+        }
+        static void Main(string[] args)
+        {
+            multiply<int> result = Multiply;
+            Console.WriteLine(result(400, 600));
+
+            multiply<string> value = Concat;
+            Console.WriteLine(value("This is ", "Result!!"));
+            Console.ReadLine();
+        }
+    }
+}
