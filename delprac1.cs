514ed2a070082a8a25ee6d2a1b160460f8d5ccd2
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-10-07 14:33:22 +0530

    Add files via upload

---
+++ delprac1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegateprac2
+{
+    public delegate void Delegateprac(string msg);
+    public delegate string Delegateprac1(string msg);
+
+    class delprac
+    {
+        public class wishem
+        {
+            public static string Wishme(string message)
+            {
+                Console.WriteLine("Hello" + message+ "Happy Birthday");
+                return message;
+            }
+        }
+
+        public class Birthday
+        {
+            public static void Hari(string message)
+            {
+                Console.WriteLine("Hello" + message + "Hari" + message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            delprac proc = new delprac();
+            Delegateprac1 del = wishem.Wishme;
+            del("Hello this is hari");
+
+            Delegateprac del1 = Birthday.Hari;
+            del("Save the day");
+
+            Delegateprac del2 = (string msg) => Console.WriteLine("Called lambda expression: " + msg);
+            del("Smile");
+            Console.ReadKey();
+
+        }
+    }
+
+}
+    
+
