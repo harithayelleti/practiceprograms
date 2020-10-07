f7e2f5a114033055bb1df2495569f40410996134
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-10-07 14:18:30 +0530

    Add files via upload

---
+++ delprac.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegateprac2
+{
+    public delegate void MyDelegate(string msg);
+
+    class delprac
+    {
+        public class ClassA
+        {
+            public static void MethodA(string message)
+            {
+                Console.WriteLine("Called ClassA.MethodA() with parameter: " + message);
+            }
+        }
+
+        public class ClassB
+        {
+            public static void MethodB(string message)
+            {
+                Console.WriteLine("Called ClassB.MethodB() with parameter: " + message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            MyDelegate del = ClassA.MethodA;
+            del("Hello World");
+
+            del = ClassB.MethodB;
+            del("Hello World");
+
+            del = (string msg) => Console.WriteLine("Called lambda expression: " + msg);
+            del("Hello World");
+            Console.ReadKey();
+
+        }
+    }
+
+}
+    
+
