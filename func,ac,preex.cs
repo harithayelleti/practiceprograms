a45daaa3ad011fd76712d49bce9677043a097b11
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-10-08 22:29:49 +0530

    Add files via upload

---
+++ func,ac,preex.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace func_pred_actex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, float,double> obj1 = (x, y) => (x * y);
+
+            double res1 = obj1.Invoke(1100, 24000);
+            Console.WriteLine(res1);
+
+
+            Func<string, string> p = name1 => "Hello" + name1;
+
+            string name = p.Invoke("Yelleti");
+            Console.WriteLine(name);
+
+            Action<float, float, float> obj2 = (x, y, z) => Console.WriteLine(x + y + z);
+
+            obj2.Invoke(10.243f, 3.142f, 66742.896f );//Non return type
+
+            Action<string> w = (wish) => Console.WriteLine( "Happy Birthday!!!!!!" + " " + wish +"staybleesed!!!!");
+
+            w.Invoke("Haritha");
+
+
+            Predicate<int> j = value =>
+            {
+                
+                if ( value > 20808)
+                    return true;
+                return false;
+            };
+
+
+            bool check = j.Invoke(56790);
+            Console.WriteLine(check);
+
+            Predicate<string> o = (name1) =>
+            {
+                //check the string whose length is greater than 4 should print true else false;
+                if (name1.Length < 10)
+                    return true;
+                return false;
+            };//New variable to store the value -return type
+
+            bool ch = o.Invoke("Maredapaally");
+            Console.WriteLine(ch);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
+        }
+    }
+}
---
+++ lambdaexpression.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambda
+{
+    class lambdaexpression
+    {
+        static void Main(string[] Args)
+        {
+            //List to store numbers
+
+            List<int> numbers = new List<int>() { 12, 24, 36, 48, 567, 72, 78, 96, 32, 120,567,678 };
+            Console.WriteLine("The elements of List are:");
+            //using Lambda expressions to calculate sum of each number in the list(z =>z + z+ z)
+            var sum = numbers.Select(z => z + z + z);
+            foreach (var num in numbers)
+            {
+                Console.WriteLine("{0}", num);
+            }
+            Console.WriteLine();
+            //using Lambda expressions to calculate square of each number in the list(x => x*x)
+
+            var square = numbers.Select(x => x * x);
+
+            foreach (var value in square)
+            {
+                Console.WriteLine("{0}", value);
+            }
+            Console.ReadKey();
+            //using Lambda expressions to calculate cube of each number in the list(x => x*x*x)
+            var qube = numbers.Select(y => y * y *y);
+
+            foreach (var number in qube)
+            {
+                Console.WriteLine("{0}",number);
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
