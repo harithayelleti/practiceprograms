018c7eaee2890ec696ed4a45f545c23217a6324f
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-10-09 14:32:05 +0530

    Add files via upload

---
+++ nonstaticthread.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public string Thread1(string name)
+        {
+            return "thread1 started";
+        }
+        public int Thread2(int i)
+        {
+            for (i = 0; i < 15; i++)
+            {
+                if (i == 10)
+                {
+                    Console.WriteLine("the thread2 is sleeping");
+
+                    Thread.Sleep(10000);
+                }
+                Console.WriteLine("the thread is at the position :" + i);
+            }
+            return i;
+        }
+        public string Thread3(string str)
+        {
+            if (str.Length > 5)
+            {
+                Console.WriteLine("the thread is sleeping for 5 seconds");
+
+                Thread.Sleep(5000);
+            }
+            return "thread3 started";
+        }
+
+        static void Main(string[] args)
+        {
+
+            Program n = new Program();
+            string s = n.Thread1("Hari");
+            Console.WriteLine(s);
+            int j = n.Thread2(5);
+            Console.WriteLine(j);
+            string a = n.Thread3("Thread programming");
+            Console.WriteLine(a);
+
+            Console.WriteLine("Main thread executed all the threads");
+                Console.ReadLine();
+
+                
+        }
+    }
+}
