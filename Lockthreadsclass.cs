224e90804d687b69d10bce1ef3876b869b2d6db0
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-10-10 21:34:58 +0530

    Add files via upload

---
+++ Lockthreadsclass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace LockThreads
+{
+    class Lockthreadsclass
+    {
+
+        public void Display()
+        {
+            lock (this)//is used to lock the other
+            {
+                Console.WriteLine("Csharp is an ");
+                Thread.Sleep(5000);
+                Console.WriteLine("Object oriented Programming Language");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Lockthreadsclass Object = new Lockthreadsclass();
+
+            //object.Display();
+
+            //Instantiated the thread with method Refrence
+            Thread t1 = new Thread(Object.Display);
+
+            Thread t2 = new Thread(Object.Display);
+            Thread t3 = new Thread(Object.Display);
+            Thread t4 = new Thread(Object.Display);
+            t1.Start(); t2.Start(); t3.Start(); t4.Start();
+            Co nsole.ReadKey();
+        }
+    } 
+}
---
+++ jointhreads.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Join_threads
+{
+    class Program
+    {
+        public static void Thread1()
+        {
+            Console.WriteLine("Thread1 is starting");
+            for (int i = 1; i < 15; i++)
+            {
+                Console.WriteLine("Thread1 is Exiting!!!!" + i);
+            }
+            Console.WriteLine("Thread3 is Exited at run method");
+        }
+        static void Sync()
+        {
+            Console.WriteLine("4-2 results released");
+            for (int i = 1; i < 15; i++)
+            {
+                Console.WriteLine("Happy to anounce" + i);
+            }
+            Console.WriteLine("I am Graduated");
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demonstartion on Join Method in Multithreading");
+            Console.WriteLine("Main Thread started");
+            //Binded the method with the method object
+            Thread T1 = new Thread(Thread1);
+            T1.Start(); 
+            T1.Join(); 
+            Thread T2 = new Thread(Sync);
+             T2.Start(); 
+             T2.Join(); 
+
+            Console.WriteLine("Main thread is exited");
+            Console.ReadKey();
+
+        }
+    }
+}
