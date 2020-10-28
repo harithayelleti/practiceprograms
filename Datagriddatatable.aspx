70dfdf8a072a76f82de7daafb85bc6a7745c0196
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-10-28 13:19:03 +0530

    Add files via upload

---
+++ Datagriddatatable.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Datagriddatatable.aspx.cs" Inherits="WebApplication4.Datagriddatatable" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DataGrid ID="DataGrid1" runat="server" OnSelectedIndexChanged="DataGrid1_SelectedIndexChanged" />
+            </div><br/><br/>
+        <div>
+            <asp:DataGrid ID="DataGrid2" runat="server" OnSelectedIndexChanged="DataGrid1_SelectedIndexChanged" />
+        </div><br/><br/>
+            <div>
+            <asp:DataGrid ID="DataGrid3" runat="server" OnSelectedIndexChanged="DataGrid1_SelectedIndexChanged" />
+        </div><br/><br/>
+        <div>
+            <asp:DataGrid ID="DataGrid4" runat="server" OnSelectedIndexChanged="DataGrid1_SelectedIndexChanged" />
+        </div><br/><br/>
+        <div>
+            <asp:DataGrid ID="DataGrid5" runat="server" OnSelectedIndexChanged="DataGrid1_SelectedIndexChanged" />
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ Datagriddatatable.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication4
+{
+    public partial class Datagriddatatable : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Establish the database connection
+            using (SqlConnection Connect = new SqlConnection("data source=. ; database = Asp.net;integrated security=SSPI"))
+            {
+              SqlDataAdapter SDA = new SqlDataAdapter("Select * from employee",Connect); //To retrieve the data from the database-student created
+                SqlDataAdapter SDA1 = new SqlDataAdapter("Select * from student", Connect);
+                SqlDataAdapter SDA2 = new SqlDataAdapter("Select * from IndianCricketTeam", Connect);
+                SqlDataAdapter SDA3 = new SqlDataAdapter("Select * from BanKTable", Connect);
+                SqlDataAdapter SDA4 = new SqlDataAdapter("Select * from IndianRailwaySystem", Connect);
+                DataSet ds = new DataSet();//To convert the data into the grid.
+
+                SDA.Fill(ds);
+                SDA1.Fill(ds);
+                SDA2.Fill(ds);
+                SDA3.Fill(ds);
+                SDA4.Fill(ds);
+
+                DataGrid1.DataSource = ds;
+                DataGrid1.DataBind();
+
+
+            }
+        }
+
+        protected void DataGrid1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
