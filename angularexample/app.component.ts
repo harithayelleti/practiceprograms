a93d81dec925616be2ab0a0226be862f0509d4e8
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-11-06 21:20:12 +0530

    Add files via upload

---
+++ angularexample/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'Welcome to Angular to the world';
+    subtitle = '.NET Core + Angular CLI v7 + Bootstrap & FontAwesome + Swagger Template';
+    titlee = 'Welcome to the world';
+    subtitlee = 'Here the angular will be dealt of';
+
+   //numbers : number[] = [1,2,3,4,5];
+
+}
