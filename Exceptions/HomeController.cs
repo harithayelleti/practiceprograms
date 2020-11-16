d0f7cb3b99bfde1ae2c67c50dfc95f8c55a167d7
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-11-16 23:18:45 +0530

    Add files via upload

---
+++ Exceptions/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HandlesErrorsInMvc.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+       
+        // GET: Home
+        public ActionResult Index()
+        {
+            
+                throw new Exception("This is a Excpetion");
+        }
+
+
+
+        public ActionResult About()
+        {
+
+            throw new Exception("This is a Excpetion");
+        }
+    }
+}
