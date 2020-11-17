9d0bbd3d122ecff20e76f77bc20af1791e25a371
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2020-11-17 19:55:44 +0530

    Add files via upload

---
+++ MvcLogin,singup/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication8.Models;
+
+namespace WebApplication8.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Home
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+
+            using (var context = new officeEntities())
+            {
+                bool isValid = context.User.Any(x => x.UserName == model.UserName && x.Passsword == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+                ModelState.AddModelError("", "Invalid UserName and password");
+                return View();
+            }
+                
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User model)
+        {
+            using (var context = new officeEntities())
+            {
+                context.User.Add(model);
+                context.SaveChanges();
+            }
+                return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
