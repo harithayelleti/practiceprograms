78808491af7ef842c249186ccef1b73f75675ec9
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2021-01-19 16:08:40 +0530

    Add files via upload

---
+++ sample proj/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using SamplePorj1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SamplePorj1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [Authorize(Roles = "admin")]
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User model, string returnUrl)
+        {
+            JobportalEntities jobportal = new JobportalEntities();
+            var dataItem = jobportal.Users.Where(x => x.Username == model.Username && x.password == model.password).First();
+            if (dataItem != null)
+            {
+                FormsAuthentication.SetAuthCookie(dataItem.Username, false);
+                if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                    && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    //return RedirectToAction("Index");
+                    return RedirectToAction("DashBoard", "DashBoard");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid shortid/password");
+                return View();
+            }
+
+        }
+        [Authorize]
+
+
+
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
---
+++ sample proj/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SamplePorj1.Models
+{
+    public class Context
+    {
+        private SqlConnection con;
+        private void connection()
+        {
+            string conn = System.Configuration.ConfigurationManager.ConnectionStrings["Jobportal"].ToString();
+            con = new SqlConnection(conn);
+        }
+    }
+}
