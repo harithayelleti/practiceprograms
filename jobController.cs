8d160f2628b91e605bfee5df61c63a23cc8841f9
harithayelleti <72141311+harithayelleti@users.noreply.github.com>
2021-01-19 15:50:26 +0530

    Add files via upload

---
+++ jobController.cs
@@ -0,0 +1,25 @@
+﻿using SamplePorj1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SamplePorj1.Controllers
+{
+    public class jobController : Controller
+    {
+        // GET: job
+        public ActionResult Search()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Search(string searching, string searching1)
+        {
+            JobportalEntities db1 = new JobportalEntities();
+            var Jobpost = db1.Jobs.Where(x => x.JobPosition.Contains(searching) || searching == null);
+            return View(Jobpost.ToList());
+        }
+    }
+}
